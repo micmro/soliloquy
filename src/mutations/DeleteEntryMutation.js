612893bd9a2a2c0cf7cc01ad5520e25d56b724aa
Michael Mrowetz <me@michaelmrowetz.com>
2017-07-19 14:52:14 +0200

    cleanup

--- src/mutations/DeleteEntryMutation.js
+++ src/mutations/DeleteEntryMutation.js
@@ -1,30 +1,4 @@
-// import {
-//   commitMutation,
-//   graphql,
-// } from 'react-relay';
-// import { ConnectionHandler } from 'relay-runtime';
 import environment from '../createRelayEnvironment'
-
-// const mutation = graphql`
-//   mutation DestroyShipMutation($input: DestroyShipData!) {
-//     destroyShip(input: $input) {
-//       destroyedShipId
-//       faction {
-//         ships {
-//           id
-//         }
-//       }
-//     }
-//   }
-// `;
-
-// const configs = [{
-//   type: 'NODE_DELETE',
-//   deletedIDFieldName: 'destroyedShipId',
-// }];
-
-
-
 import {
   commitMutation,
   graphql,
@@ -42,7 +16,6 @@ const configs = [{
   type: 'NODE_DELETE',
   deletedIDFieldName: 'id',
 }];
-
 
 function DeleteEntryMutation(entryID) {
   const variables = {
